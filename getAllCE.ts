7084dd1f67b2e72beb781439ca53b6c1a66c01c3
bahrus <anderson.bruce.b@gmail.com>
2018-11-23 16:48:01 -0500

    ...

--- getAllCE.ts
+++ getAllCE.ts
@@ -3,6 +3,7 @@ console.log(elems.length);
 for(let i = 0, ii = elems.length; i < ii; i++){
     const elem = elems[i];
     const ln = elem.localName;
+    console.log(ln);
     if(ln.indexOf('-') > -1){
         if(customElements.get(ln)){
             console.log(ln);
