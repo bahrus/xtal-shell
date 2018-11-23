acc0f9db5d7e2fb687d826be945c0321ccdca6eb
bahrus <anderson.bruce.b@gmail.com>
2018-11-23 16:59:12 -0500

    ...

--- getAllCE.js
+++ getAllCE.js
@@ -1,12 +1,21 @@
-const elems = document.body.getElementsByTagName("*");
-console.log(elems.length);
-for (let i = 0, ii = elems.length; i < ii; i++) {
-    const elem = elems[i];
-    const ln = elem.localName;
-    if (ln.indexOf('-') > -1) {
-        if (customElements.get(ln)) {
-            console.log(ln);
+function getEls(container) {
+    const elems = document.body.getElementsByTagName("*");
+    console.log(elems.length);
+    for (let i = 0, ii = elems.length; i < ii; i++) {
+        const elem = elems[i];
+        const ln = elem.localName;
+        if (ln.indexOf('-') > -1) {
+            if (customElements.get(ln)) {
+                console.log(ln);
+            }
+        }
+        else if (ln === 'iframe') {
+            try {
+                getEls(elem.contentWindow.document.body);
+            }
+            catch (e) { }
         }
     }
 }
+getEls(document.body);
 //# sourceMappingURL=getAllCE.js.map
