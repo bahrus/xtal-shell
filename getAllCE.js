3cbc9fdb76eac807f66b62bc5046c7df1da3dc70
bahrus <anderson.bruce.b@gmail.com>
2018-11-23 16:40:40 -0500

    ...

---
+++ getAllCE.js
@@ -0,0 +1,11 @@
+const elems = document.body.getElementsByTagName("*");
+for (let i = 0, ii = elems.length; i++; i < ii) {
+    const elem = elems[i];
+    const ln = elem.localName;
+    if (ln.indexOf('-') > -1) {
+        if (customElements.get(ln)) {
+            console.log(ln);
+        }
+    }
+}
+//# sourceMappingURL=getAllCE.js.map
